67f35fc42d2cfb7577e95b5659318279a4b1cf89
maringeorgiana18 <72292423+maringeorgiana18@users.noreply.github.com>
2023-01-08 21:55:24 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,1 @@
-docker rm -f bookshop
-docker rm -f booklibrary
-docker rm -f authserver
-
-docker image rm proiect_idp_bookservice_booklibrary
-docker image rm proiect_idp_bookservice_bookshop
-docker image rm proiect_idp_bookservice_authserver
-
-cd Bookshop
-mvn clean install -DskipTests
-cd ..
-cd Booklibrary
-mvn clean install -DskipTests
-cd ..
-cd Authserver
-mvn clean install -DskipTests
-
 docker-compose up -d
